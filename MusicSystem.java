af8f2c6a98585828138eca29a7af5cc74c2e2dc7
Isabel <59679787+Isabelppz@users.noreply.github.com>
2020-01-27 23:29:02 -0800

    Add files via upload

---
+++ MusicSystem.java
@@ -0,0 +1,8 @@
+public class MusicSystem{
+    public static void main(String args[]){
+        
+        Singer p1 = new Singer("Beyonce",38,"USA",800000.00);;
+        p1.printSinger();
+
+    }
+}
